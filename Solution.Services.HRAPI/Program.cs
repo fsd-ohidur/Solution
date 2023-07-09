6ded9ffbd201d1bd8ab309bfa50f358597397707
Ohid <ohid@ANGKO.NET>
2023-07-09 13:00:10 +0600

    Project Basic Structure Complete

--- Solution.Services.HRAPI/Program.cs
+++ Solution.Services.HRAPI/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Solution.Services.HRAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
